48ac1bdfa3125a30ded0bd03f1e6c34afb94c901
Sameer Chauhan <sameerchauhan212204@gmail.com>
2025-12-29 22:59:03 +0530

    A simple endpoint

--- app/main.py
+++ app/main.py
@@ -1,3 +1,7 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI()   # initialise a fastapi app 
+
+@app.get("/")     # creating a path operations (GET, POST)
+async def root(): # A async functions defines the path operation
+    return {"message": "Hello World!"} # returning the content
