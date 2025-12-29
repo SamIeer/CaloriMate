c2b09783499930cdbf507613483695081ce6a978
Sameer Chauhan <sameerchauhan212204@gmail.com>
2025-12-29 23:10:26 +0530

    Working on it

--- app/main.py
+++ app/main.py
@@ -5,3 +5,5 @@ app = FastAPI()   # initialise a fastapi app
 @app.get("/")     # creating a path operations (GET, POST)
 async def root(): # A async functions defines the path operation
     return {"message": "Hello World!"} # returning the content
+
+WOrking should be done 
