a8d3663266005a27ea40a45ad276109f9db145bd
Sameer Chauhan <sameerchauhan212204@gmail.com>
2026-01-01 23:47:22 +0530

    endpoints added

--- app/main.py
+++ app/main.py
@@ -4,6 +4,15 @@ app = FastAPI()   # initialise a fastapi app
 
 @app.get("/")     # creating a path operations (GET, POST)
 async def root(): # A async functions defines the path operation
-    return {"message": "Hello World!"} # returning the content
+    return {"message": "Wellcome to  CaloriMate API - Predict your calori "} # returning the content
 
-WOrking should be done 
+@app.get("/health")
+def health_check():
+    return {"status": "healthy"}
+
+@app.post("/pedict")
+def predict_calori(data: DataInput):
+    input_data = pd.dataframe([data.model_dump()])
+    prediction = model.predict(input_data)[0]
+    return {"Your calori Burn": round(float(prediction),2)}
+
