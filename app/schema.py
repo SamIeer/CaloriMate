1756716891ad665c17cc0b270431af17cf154fd0
Sameer Chauhan <sameerchauhan212204@gmail.com>
2026-01-05 18:48:15 +0530

    Model created

--- app/schema.py
+++ app/schema.py
@@ -1,4 +1,14 @@
 from pydantic import BaseModel
 
 class ModelInput(BaseModel):
-    pass
+    Sex : str
+    Age : int
+    Height : float
+    Weight : float
+    Duration : float
+    Heart_Rate : float
+    Body_Temp : float
+
+'''
+This insures clean input, validate data - no missing or invalid field when API get hit
+'''
