c7f578c1f2daaaea8514d21c40e21226bd4ac4b1
Sameer Chauhan <sameerchauhan212204@gmail.com>
2026-01-05 18:49:33 +0530

    Endpoints created

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
+import pandas as pd
+from app.schema import ModelInput
+import joblib 
 
 app = FastAPI()   # initialise a fastapi app 
+
+model = joblib.load("../Model/calorie_model.joblib")
 
 @app.get("/")     # creating a path operations (GET, POST)
 async def root(): # A async functions defines the path operation
@@ -10,9 +15,9 @@ async def root(): # A async functions defines the path operation
 def health_check():
     return {"status": "healthy"}
 
-# @app.post("/pedict")
-# def predict_calori(data: DataInput):
-#     input_data = pd.dataframe([data.model_dump()])
-#     prediction = model.predict(input_data)[0]
-#     return {"Your calori Burn": round(float(prediction),2)}
+@app.post("/pedict")
+def predict_calori(data: ModelInput):
+    input_data = pd.dataframe([data.model_dump()])
+    prediction = model.predict(input_data)[0]
+    return {"Your calori Burn": round(float(prediction),2)}
 
