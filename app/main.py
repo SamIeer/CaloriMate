0d3cfdd2cba076ff65ce2d7e8d6dde74d85b7136
Sameer Chauhan <sameerchauhan212204@gmail.com>
2026-01-18 22:40:50 +0530

    Fixin root

--- app/main.py
+++ app/main.py
@@ -8,20 +8,20 @@ app = FastAPI()   # initialise a fastapi app
 model = joblib.load("Model/calorie_model.pkl")
 
 @app.get("/")     # creating a path operations (GET, POST)
-async def root(): # A async functions defines the path operation
+def root(): # A async functions defines the path operation
     return {"message": "Wellcome to  CaloriMate API - Predict your calori "} # returning the content
 
 @app.get("/health") # EndPoint for health
 def health_check(): 
     return {"status": "healthy"}
 
-@app.post("/pedict") #Endpoint for predict 
+@app.post("/predict") #Endpoint for predict 
 def predict_calori(data: ModelInput):
     # Convert request into dataframe
     input_data = pd.DataFrame([data.model_dump()])
     # Predict
     prediction = model.predict(input_data)[0]
-    return {"Your calori Burn": round(float(prediction),2)}
+    return {"Calories_Burn": round(float(prediction),2)}
 
 # This is a working app 
 # Don't slack off bro 
